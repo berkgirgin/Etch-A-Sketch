37698e56fb3f7ca91de7ef3b53c4de456bd0b6c9
berk-girgin <berk.girgin@zattoo.com>
2022-11-19 23:26:01 +0100

    added color and css

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
 const container_sketch = document.querySelector(".container_sketch");
-const dimensionButton = document.querySelector("button.dimensionButton");
-const resetButton = document.querySelector("button.reset");
+var isBlackOrColorOrDelete = "black_selected";
 
+const resetButton = document.querySelector("button.reset");
+resetButton.addEventListener("click", function() {
+    createGrids(10);
+    isBlackOrColorOrDelete = "black_selected";
+});
+
+const button10 = document.querySelector(".grid10");
+button10.addEventListener("click", function() {createGrids(10)});
+
+const button20 = document.querySelector(".grid20");
+button20.addEventListener("click", function() {createGrids(20)});
+
+const button30 = document.querySelector(".grid30");
+button30.addEventListener("click", function() {createGrids(30)});
+
+const button40 = document.querySelector(".grid40");
+button40.addEventListener("click", function() {createGrids(40)});
+
+const button50 = document.querySelector(".grid50");
+button50.addEventListener("click", function() {createGrids(50)});
+
+const button60 = document.querySelector(".grid60");
+button60.addEventListener("click", function() {createGrids(60)});
+
+const eraseButton = document.querySelector(".erase_button");
+eraseButton.addEventListener("click", function() {isBlackOrColorOrDelete = "erase_selected";})
+
+const blackMarker = document.querySelector(".black_marker");
+blackMarker.addEventListener("click", function() {isBlackOrColorOrDelete = "black_selected";})
+
+const colorMarker = document.querySelector(".color_marker");
+colorMarker.addEventListener("click", function() {isBlackOrColorOrDelete = "color_selected";})
+
+
+// setting up the start with 10x10 grid
 createGrids(10);
 
 
-resetButton.addEventListener("click", function() {createGrids(10);})
+function mouseoverFunc(x) {
 
-dimensionButton.addEventListener( "click", function() {
-    x = Number(prompt("please enter your dimension"));
-
-    if ( isNaN(x) ) {
-        alert("you should enter a number");
-        return;
-    } else if (x === 0) {
-        alert("you can't enter zero or nothing");
-        return;
-    } else if (x<1 || x>60) {
-        alert("It should be between 1 and 60");
-        return;
-    } else {
-        createGrids(x);
+    if (isBlackOrColorOrDelete === "black_selected") {
+        x.setAttribute("style", "background-color: black;");
+    } else if (isBlackOrColorOrDelete === "color_selected") {
+        randomColor = randomColorGenerator();
+        x.setAttribute("style", `background-color: ${randomColor};`);
+    } else if (isBlackOrColorOrDelete === "erase_selected") {
+        x.removeAttribute("style");
     }
-} );
+}
 
+function randomColorGenerator() {
+    // create a random rgb color
+    function generateRandomNumber() {
+        // generate a random number between 1 and 255
+        let result = Math.floor(Math.random() * 255) + 1;
+        return result;
+      }
+    x = generateRandomNumber();  
+    y = generateRandomNumber();  
+    z = generateRandomNumber();  
+    return `rgb(${x}, ${y}, ${z})`;
+}
 
 
 function createGrids(dimension) {
@@ -42,7 +81,7 @@ function createGrids(dimension) {
     } 
 
     const boxes = document.querySelectorAll(".box");
-    boxes.forEach( (box) => box.addEventListener("mouseover", function() {this.classList.add("mouseover")} ));
+    boxes.forEach( (box) => box.addEventListener("mouseover", function() {mouseoverFunc(this);} ));
 
 }
 
